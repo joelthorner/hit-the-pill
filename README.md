fea84dc1c6fc301b9c78aa39b162d7cb60d949ac
Joel Torner <joeltorner@gmail.com>
2017-09-29 22:35:32 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+
+Hit-the-pill
+==========
+## Testing
+[localStorage](https://developer.mozilla.org/en-US/docs/Web/API/Storage/LocalStorage)
+[KUTE.js](http://thednp.github.io/kute.js/index.html)
+[SVG properties](https://developer.mozilla.org/en-US/docs/Web/SVG)
+
+## Demo
+Demo and doc : [http://joelthorner.github.io/hit-the-pill](http://joelthorner.github.io/hit-the-pill/)
+
+## Credits
+BG js animation by [JoanClaret](https://github.com/JoanClaret/html5-canvas-animation/blob/master/js/color.js)
+Stars with THREE.js [codepen](https://codepen.io/seanseansean/pen/vEjOvy)
+Elastic svg button [Codrops](https://tympanus.net/Development/ElasticSVGElements/button.html)
--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,10 @@
+/* Joelthorner 2017 - http://joelthorner.github.io/hit-the-pill/ */
+
 const ITEMS = 8;
+var morph1 = null;
 
 function updateList(){
+	var findeds = 0;
 	for (var i = 0; i < ITEMS; i++) {
 		var finded = localStorage.getItem('i-' + i + '-finded');
 		var n = localStorage.getItem('i-' + i + '-n');
@@ -8,8 +12,21 @@ function updateList(){
 		if (finded == "true") {
 			$('.menu ul .i-' + i + ' svg')
 				.html( $('#source symbol').eq(i).html() );
-			$('.menu ul .i-' + i + ' .n').text(n)
+			$('.menu ul .i-' + i + ' .n').text(n);
+			findeds++;
 		}
+	}
+	
+	if (findeds == ITEMS || localStorage.getItem('WINNER') == 'true') {
+		localStorage.setItem('WINNER', true);
+		
+		if (morph1) morph1.stop();
+		morph1 = KUTE.to('#path-1',  { path : $('#winner path').attr('d') }, { morphPrecision : 3, duration: 500 });//.start();
+		!morph1.playing && morph1.start();
+
+		var myTextTween = KUTE.to('#winner', { text: 'Winner'}).start();;
+	}else{
+		localStorage.setItem('WINNER', false);
 	}
 }
 
@@ -21,10 +38,11 @@ function initStorage(){
 }
 
 $(document).ready(function() {
-
+	updateGradient();
+	// setInterval(updateGradient, 35);
 
 	var saveData = localStorage.getItem('saveData');
-	var min = 0, max = ITEMS -1, prev, rand, newPath, morph1;
+	var min = 0, max = ITEMS -1, prev, rand, newPath;//, morph1;
 
 	if (saveData == 'true') {
 		$('html').addClass('open-init');
@@ -36,31 +54,33 @@ $(document).ready(function() {
 
 	updateList();
 
-	$('#svg').click(function(event) {
+	$('#svg').on('click.change', function(event) {
+		if(localStorage.getItem('WINNER') != 'true')	{
 
-		console.time('a');
-		if (morph1) morph1.stop();
+			if (morph1) morph1.stop();
 
-		rand = Math.floor(Math.random() * (max - min + 1) + min);
-
-		while(prev == rand){
-			console.log(prev, rand);
 			rand = Math.floor(Math.random() * (max - min + 1) + min);
-		}
-		prev = rand;
-		localStorage.setItem('prev', prev);
-		localStorage.setItem('i-' + rand + '-finded', true);
-		localStorage.setItem('i-' + rand + '-n', parseInt(localStorage.getItem('i-' + rand + '-n')) + 1 );
-		newPath = $('#source symbol').eq(rand).find('path');
 
-		
-		$('html').addClass('open');
-		localStorage.setItem('saveData', true);
-		
-		updateList();
-		morph1 = KUTE.fromTo('#path-1',  { path : '#path-1' }, { path : newPath.attr('d') }, { morphPrecision : 3, duration: 1000 }).start();
-		console.timeEnd('a');
-// console.log(a);
+			while(prev == rand){
+				console.log(prev, rand);
+				rand = Math.floor(Math.random() * (max - min + 1) + min);
+			}
+			prev = rand;
+			localStorage.setItem('prev', prev);
+			localStorage.setItem('i-' + rand + '-finded', true);
+			localStorage.setItem('i-' + rand + '-n', parseInt(localStorage.getItem('i-' + rand + '-n')) + 1 );
+			newPath = $('#source symbol').eq(rand).find('path');
+
+			
+			$('html').addClass('open');
+			localStorage.setItem('saveData', true);
+			
+			morph1 = KUTE.to('#path-1',  { path : newPath.attr('d') }, { morphPrecision : 3, duration: 500 });//.start();
+			!morph1.playing && morph1.start();
+			
+			updateList();
+		}
+
 	});
 
 	
@@ -74,6 +94,7 @@ $(document).ready(function() {
 
 		localStorage.removeItem("saveData");
 		localStorage.removeItem("prev");
+		localStorage.removeItem('WINNER');
 		for (var i = 0; i < ITEMS; i++) {
 			localStorage.removeItem('i-' + i + '-finded');
 			localStorage.removeItem('i-' + i + '-n');
